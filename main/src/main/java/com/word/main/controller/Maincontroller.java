832faa4fa99a97f3270c1ee5e806b1dfd847b93a
NolimitL <36302876+NolimitL@users.noreply.github.com>
2019-12-23 00:01:32 +0100

    Have maked PUT,POST,UPDATE,DELETE mapping. Simply application

--- main/src/main/java/com/word/main/controller/Maincontroller.java
+++ main/src/main/java/com/word/main/controller/Maincontroller.java
@@ -3,25 +3,35 @@ package com.word.main.controller;
 import com.word.main.model.TopicOfBooks;
 import com.word.main.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class Maincontroller {
 
-    @Autowired
+    @Autowired //Авто-связь с классами
     private TopicService topicService;
 
-    @RequestMapping("/topic")
+    @RequestMapping("/topic")  //@RequestMapping - отображение запроса (Request-запрос, Mapping-отображение)
     public List<TopicOfBooks> getAllTopics() {
         return topicService.getTopic();
     }
     @RequestMapping("/topic/{amountbook}")
-    public TopicOfBooks getTopic(@PathVariable("amountbook") String id){
+    public TopicOfBooks getTopic(@PathVariable("amountbook") String id){  //@PathVariable - подходящая переменная из ссылки переводиться в вводимый параметр
         return topicService.ChooseTopic(id);
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "/topic") //вызывает метод из аннотации
+    public void addTopicOfBook(@RequestBody TopicOfBooks topicOfBooks){
+        topicService.addTopic(topicOfBooks);
+    }
+    @RequestMapping(method = RequestMethod.PUT, value = "/topic/{amountbook}") //вызывает метод из аннотации
+    public void updateTopicOfBook(@RequestBody TopicOfBooks topicOfBooks, @PathVariable("amountbook") String id){
+        topicService.updateTopic(id, topicOfBooks);
+    }
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topic/{amountbook}")
+    public void deleteTopic(@PathVariable("amountbook") String id) {
+         topicService.deleteTopic(id);
     }
 }
 
@@ -35,4 +45,3 @@ public class Maincontroller {
 //        int[] arr = {1,2,3,3};
 //        return arr;
 //    }
-
--- main/src/main/java/com/word/main/service/TopicService.java
+++ main/src/main/java/com/word/main/service/TopicService.java
@@ -3,16 +3,17 @@ package com.word.main.service;
 import com.word.main.model.TopicOfBooks;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<TopicOfBooks> topic = Arrays.asList(
-                new TopicOfBooks(1,"The Adventures of Tom Sawyer", "Mark Twain"),
-                new TopicOfBooks( 2,"The Catcher in the Rye", "David Salinger")
-        );
+    private List<TopicOfBooks> topic = new ArrayList<>(Arrays.asList(
+            new TopicOfBooks(1,"The Adventures of Tom Sawyer", "Mark Twain"),
+            new TopicOfBooks( 2,"The Catcher in the Rye", "David Salinger")
+        ));
 
     public List<TopicOfBooks> getTopic() {
         return topic;
@@ -20,5 +21,23 @@ public class TopicService {
 
     public TopicOfBooks ChooseTopic(String id){
         return topic.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(TopicOfBooks topicOfBooks) {
+        topic.add(topicOfBooks);
+    }
+
+    public void updateTopic(String id, TopicOfBooks topicOfBooks) {
+        for (int i = 0; i < topic.size(); i++) {
+            TopicOfBooks t = topic.get(i);
+            if (t.getId().equals(id)){
+                topic.set(i, topicOfBooks);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topic.removeIf(t -> t.getId().equals(id));
     }
 }
