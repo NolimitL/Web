611dec72a5e3a81e2a242fb33783093f800d1d74
Daniil Durnev <dandurnev1@gmail.com>
2019-12-24 09:58:38 -0800

    problem fix

--- main/src/main/java/com/word/main/model/ITopicRepository.java
+++ main/src/main/java/com/word/main/model/ITopicRepository.java
@@ -1,6 +1,11 @@
 package com.word.main.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 public interface ITopicRepository extends CrudRepository<TopicOfBooks, String> {
+    List<TopicOfBooks> findAll();
+    TopicOfBooks findById();
 }
